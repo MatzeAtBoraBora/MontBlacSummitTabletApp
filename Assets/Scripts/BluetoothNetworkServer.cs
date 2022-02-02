82abc049074d3848b6345d6c0ce1ab6c0bfcdc46
MatzeAtBoraBora <94051254+MatzeAtBoraBora@users.noreply.github.com>
2022-02-02 13:57:54 +0100

    Refactored our own Bluetooth Plugin, Add Fonts and some Assets

--- Assets/Scripts/BluetoothNetworkServer.cs
+++ Assets/Scripts/BluetoothNetworkServer.cs
@@ -3,31 +3,95 @@ using UnityEngine;
 using UnityEngine.UI;
 using Shatalmic;
 using System;
-using System.Text;
+using UnityEngine.Events;
 
 public class BluetoothNetworkServer : MonoBehaviour
 {
+	/* External UI attributes */
+	[Space]
+	[Header("Network Settings")]
+	public String networkName = "summit";
+	public String clientName = "summitClient";
+	public bool isClient = true;
+
+	[Space]
+	[Header("Action Buttons")]
+	public GameObject ButtonStartServer;
+	public GameObject ButtonStartClient;
+	public GameObject ButtonStopNetwork;
+	public GameObject ButtonSendTestData;
+
+	
+	[Space]
+	[Header("Debug & Explaination Layer")]
+	public Text ServerOutputText;
+	public GameObject NetworkDialog;
+	public GameObject BluetoothSettingsDialog;
+	public GameObject ExplainationDialog;
+
+
+	[Space]
+	[Header("Custom Events")]
+	public UnityEvent OnStartServer;
+	public UnityEvent OnStartClient;
+	public UnityEvent OnClientConnected;
+	[System.Serializable]
+	public class MessageEvent : UnityEvent<String>
+	{
+	}
+	public MessageEvent OnMessageReceived;
+	public UnityEvent OnStopServer;
+
+
+	/* Internal Server attributes */
 	private Networking networking = null;
 	private List<Networking.NetworkDevice> connectedDeviceList = null;
 	private bool isServer = true;
 	private bool writeDeviceBytes = false;
 	private byte[] bytesToWrite = null;
 	private int deviceToWriteIndex = 0;
-	private bool sendingCubeRotation = false;
-	private Quaternion newCubeRotation = Quaternion.identity;
 	private Networking.NetworkDevice deviceToSkip = null;
 
-	public GameObject Cube;
-	public Text TextStatus;
-	public GameObject BluetoothSettingsDialog;
 
-	public void OnApplicationQuit()
+	// Use this for initialization
+	void Start()
 	{
-		if (Application.isEditor)
+		BluetoothSettingsDialog.SetActive(false);
+		if (isClient)
 		{
-			BluetoothLEHardwareInterface.DeInitialize(() =>
+			ButtonStartClient.SetActive(true);
+			ButtonStartServer.SetActive(false);
+		}
+		else
+		{
+			ButtonStartServer.SetActive(true);
+			ButtonStartClient.SetActive(false);
+		}
+
+		ButtonStopNetwork.SetActive(false);
+		ButtonSendTestData.SetActive(false);
+		
+		if (networking == null)
+		{
+			networking = GetComponent<Networking>();
+			networking.Initialize((error) =>
 			{
-				Console.WriteLine("Deinit done");
+				// see if we got a standard bluetooth error
+				if (bluetoothErrors.Contains(error))
+				{
+					// if the error is one of these then show the dialog asking the user
+					// to enable bluetooth
+					if (error.Equals(bluetoothErrors[0]) || error.Equals(bluetoothErrors[1]))
+						BluetoothSettingsDialog.SetActive(true);
+				}
+
+				BluetoothLEHardwareInterface.Log("Error: " + error);
+			}, (message) =>
+			{
+				if (ServerOutputText != null)
+					ServerOutputText.text = message;
+
+				BluetoothLEHardwareInterface.Log("Message: " + message);
 			});
 		}
 	}
@@ -54,7 +118,6 @@ public class BluetoothNetworkServer : MonoBehaviour
 					if (!writeDeviceBytes)
 					{
 						bytesToWrite = null;
-						sendingCubeRotation = false;
 					}
 				};
 
@@ -69,99 +132,7 @@ public class BluetoothNetworkServer : MonoBehaviour
 				}
 			}
 		}
-
-		if (newCubeRotation != Quaternion.identity)
-		{
-			if (Cube != null)
-				Cube.transform.rotation = newCubeRotation;
-			newCubeRotation = Quaternion.identity;
-		}
 	}
-
-	protected void SendCubeRotation(Quaternion rotation, Networking.NetworkDevice device = null)
-	{
-		if (!sendingCubeRotation)
-		{
-			sendingCubeRotation = true;
-
-			var bytes = new byte[16];
-			Array.Copy(BitConverter.GetBytes(rotation.x), 0, bytes, 0, 4);
-			Array.Copy(BitConverter.GetBytes(rotation.y), 0, bytes, 4, 4);
-			Array.Copy(BitConverter.GetBytes(rotation.z), 0, bytes, 8, 4);
-			Array.Copy(BitConverter.GetBytes(rotation.w), 0, bytes, 12, 4);
-
-			if (isServer)
-			{
-				if (device == null)
-				{
-					if (connectedDeviceList != null)
-					{
-						if (connectedDeviceList.Count == 1)
-						{
-							if (deviceToSkip == null)
-							{
-								networking.WriteDevice(connectedDeviceList[0], bytes, () =>
-								{
-									sendingCubeRotation = false;
-								});
-							}
-							else
-							{
-								deviceToSkip = null;
-								sendingCubeRotation = false;
-							}
-						}
-						else
-						{
-							deviceToWriteIndex = 0;
-							writeDeviceBytes = true;
-							bytesToWrite = bytes;
-						}
-					}
-					else if (deviceToSkip != null)
-						deviceToSkip = null;
-				}
-				else
-				{
-					networking.WriteDevice(device, bytes, () =>
-					{
-						sendingCubeRotation = false;
-					});
-				}
-			}
-			else
-			{
-				networking.SendFromClient(bytes);
-				sendingCubeRotation = false;
-			}
-		}
-	}
-
-	int count = 0;
-	protected void ParseCubePosition(byte[] bytes)
-	{
-		if (count > 0 || (bytes[0] == '0' && bytes[1] == '1' && bytes[2] == '2' && bytes[3] == '3' && bytes[4] == '4'))
-		{
-			count += bytes.Length;
-			BluetoothLEHardwareInterface.Log("Large bytes: " + count.ToString() + " : " + BitConverter.ToString(bytes));
-			if (count >= 300)
-				count = 0;
-		}
-		else
-		{
-			newCubeRotation = new Quaternion(BitConverter.ToSingle(bytes, 0), BitConverter.ToSingle(bytes, 4), BitConverter.ToSingle(bytes, 8), BitConverter.ToSingle(bytes, 12));
-
-			if (isServer)
-				SendCubeRotation(newCubeRotation);
-		}
-	}
-
-	public String networkName = "summit";
-	public String clientName = "summitClient";
-	public GameObject ButtonStartServer;
-	public GameObject ButtonStartClient;
-	public GameObject ButtonStopNetwork;
-	public GameObject ButtonSendTestData;
 
 	public void OnButton(Button button)
 	{
@@ -169,19 +140,15 @@ public class BluetoothNetworkServer : MonoBehaviour
 		{		
 			case "ButtonStartServer":
 				
-					TextStatus.text = "Start Server";
+					ServerOutputText.text = "Start Server";
 
 					deviceToSkip = null;
 					deviceToWriteIndex = 0;
-					sendingCubeRotation = false;
 					isServer = true;
 					ButtonStartServer.SetActive(false);
 					ButtonStartClient.SetActive(false);
 					ButtonStopNetwork.SetActive(true);
 					ButtonSendTestData.SetActive(true);
-
-					if (Cube != null)
-						Cube.SetActive(false);
 
 					networking.StartServer(networkName, (connectedDevice) =>
 					{
@@ -192,12 +159,11 @@ public class BluetoothNetworkServer : MonoBehaviour
 						{
 							connectedDeviceList.Add(connectedDevice);
 
-							if (Cube != null)
-							{
-								Cube.SetActive(true);
-								SendCubeRotation(Cube.transform.rotation, connectedDevice);
-							}
+							//We are connected
+							OnClientConnected.Invoke();
 						}
+						//Start Server
+						OnStartServer.Invoke();
 					}, (disconnectedDevice) =>
 					{
 						if (connectedDeviceList != null && connectedDeviceList.Contains(disconnectedDevice))
@@ -205,7 +171,8 @@ public class BluetoothNetworkServer : MonoBehaviour
 					}, (dataDevice, characteristic, bytes) =>
 					{
 						deviceToSkip = dataDevice;
-						ParseCubePosition(bytes);
+						//We are getting bytes in
+						ReadOutBytes(bytes);
 					});
 				
 				break;
@@ -213,11 +180,11 @@ public class BluetoothNetworkServer : MonoBehaviour
 			case "ButtonStartClient":
 				if (string.IsNullOrEmpty(networkName) || string.IsNullOrEmpty(clientName))
 				{
-					TextStatus.text = "Enter both a network and client name";
+					ServerOutputText.text = "Enter both a network and client name";
 				}
 				else
 				{
-					TextStatus.text = "Start Client";
+					ServerOutputText.text = "Start Client";
 
 					isServer = false;
 					ButtonStartServer.SetActive(false);
@@ -225,17 +192,13 @@ public class BluetoothNetworkServer : MonoBehaviour
 					ButtonStopNetwork.SetActive(true);
 					ButtonSendTestData.SetActive(true);
 
-					if (Cube != null)
-						Cube.SetActive(false);
-
 					networking.StartClient(networkName, clientName, () =>
 					{
 						networking.StatusMessage = "Started advertising";
+						OnStartClient.Invoke();
 					}, (clientName, characteristic, bytes) =>
 					{
-						if (Cube != null)
-							Cube.SetActive(true);
-						ParseCubePosition(bytes);
+						ReadOutBytes(bytes);
 					});
 				}
 				break;
@@ -251,9 +214,7 @@ public class BluetoothNetworkServer : MonoBehaviour
 						ButtonStartClient.SetActive(true);
 						ButtonStopNetwork.SetActive(false);
 						ButtonSendTestData.SetActive(false);
-
-						if (Cube != null)
-							Cube.SetActive(false);
+						OnStopServer.Invoke();
 					});
 				}
 				else
@@ -264,23 +225,14 @@ public class BluetoothNetworkServer : MonoBehaviour
 						ButtonStartClient.SetActive(true);
 						ButtonStopNetwork.SetActive(false);
 						ButtonSendTestData.SetActive(false);
-
-						if (Cube != null)
-							Cube.SetActive(false);
+						OnStopServer.Invoke();
 					});
 				}
 				break;
 
 			case "ButtonSendTestData":
 				{
-					var bytes = new byte[300];
-					bytes[0] = (byte)'0';
-					bytes[1] = (byte)'1';
-					bytes[2] = (byte)'2';
-					bytes[3] = (byte)'3';
-					bytes[4] = (byte)'4';
-					for (int i = 5; i < 300; i++)
-						bytes[i] = (byte)('A' + (i % 26));
+					byte[] bytes = System.Text.Encoding.UTF8.GetBytes("Hello from Client:"+isClient);
 
 					if (isServer)
 					{
@@ -292,13 +244,13 @@ public class BluetoothNetworkServer : MonoBehaviour
 								{
 									networking.WriteDevice(connectedDeviceList[0], bytes, () =>
 									{
-										sendingCubeRotation = false;
+										//we are sending data in our channel
 									});
 								}
 								else
 								{
 									deviceToSkip = null;
-									sendingCubeRotation = false;
+									//we are not writing in our channel
 								}
 							}
 							else
@@ -313,8 +265,8 @@ public class BluetoothNetworkServer : MonoBehaviour
 					}
 					else
 					{
+						//sending out test data
 						networking.SendFromClient(bytes);
-						sendingCubeRotation = false;
 					}
 				}
 				break;
@@ -326,6 +278,23 @@ public class BluetoothNetworkServer : MonoBehaviour
 		BluetoothSettingsDialog.SetActive(false);
 	}
 
+	int count = 0;
+	private void ReadOutBytes(byte[] bytes) {
+		ServerOutputText.text = "Getting Bytes in!";
+		String bytesAsString = System.Text.Encoding.UTF8.GetString(bytes);
+		OnMessageReceived.Invoke(bytesAsString);
+	}
+	public void OnApplicationQuit()
+	{
+		if (Application.isEditor)
+		{
+			BluetoothLEHardwareInterface.DeInitialize(() =>
+			{
+				Console.WriteLine("Deinit done");
+			});
+		}
+	}
+
 	List<string> bluetoothErrors = new List<string>
 	{
 		"Bluetooth LE Not Enabled",
@@ -333,58 +302,4 @@ public class BluetoothNetworkServer : MonoBehaviour
 		"Bluetooth LE Not Available",
 		"Bluetooth LE Not Supported",
 	};
-
-	// Use this for initialization
-	void Start()
-	{
-		BluetoothSettingsDialog.SetActive(false);
-		ButtonStartServer.SetActive(true);
-		ButtonStartClient.SetActive(true);
-		ButtonStopNetwork.SetActive(false);
-		ButtonSendTestData.SetActive(false);
-		if (Cube != null)
-			Cube.SetActive(false);
-
-		if (networking == null)
-		{
-			networking = GetComponent<Networking>();
-			networking.Initialize((error) =>
-			{
-				// see if we got a standard bluetooth error
-				if (bluetoothErrors.Contains(error))
-				{
-					// if the error is one of these then show the dialog asking the user
-					// to enable bluetooth
-					if (error.Equals(bluetoothErrors[0]) || error.Equals(bluetoothErrors[1]))
-						BluetoothSettingsDialog.SetActive(true);
-
-					// TODO: I leave it to the plugin user to determine how to handle the other errors
-
-					// TODO: You will also want to restructure your code so that when the app comes back from setting
-					//       it will retry initializing. Some users like to have a different scene for that.
-				}
-
-				BluetoothLEHardwareInterface.Log("Error: " + error);
-			}, (message) =>
-			{
-				if (TextStatus != null)
-					TextStatus.text = message;
-
-				BluetoothLEHardwareInterface.Log("Message: " + message);
-			});
-		}
-
-		if (Cube != null)
-		{
-			var mouseDrageRotate = Cube.GetComponent<MouseDragRotate>();
-			if (mouseDrageRotate != null)
-			{
-				mouseDrageRotate.OnMouseEvent = (rotation) =>
-				{
-					deviceToSkip = null;
-					SendCubeRotation(rotation);
-				};
-			}
-		}
-	}
 }
